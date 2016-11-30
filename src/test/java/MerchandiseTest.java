6e1a82581b750aa7c17a4ddc66af6c1e3351812d
Matt Chowning <mchowning@gmail.com>
2016-11-30 17:41:54 -0500

    REFACTOR | tweak to test to improve readability

--- src/test/java/MerchandiseTest.java
+++ src/test/java/MerchandiseTest.java
@@ -6,8 +6,9 @@ public class MerchandiseTest {
 
     @Test
     public void initializeMerchandiseWithPrice() {
-        Merchandise merchandise = new Merchandise(100);
-        int price = merchandise.getPrice();
-        assertEquals(100, price);
+        int inputPrice = 100;
+        Merchandise merchandise = new Merchandise(inputPrice);
+        int outputPrice = merchandise.getPrice();
+        assertEquals(inputPrice, outputPrice);
     }
 }
