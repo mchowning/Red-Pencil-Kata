5a31d3a6a79b446f92a824f98041c9f8cc290879
Matt Chowning <mchowning@gmail.com>
2016-11-30 17:40:12 -0500

    RED | require return of initialized price value

--- src/test/java/MerchandiseTest.java
+++ src/test/java/MerchandiseTest.java
@@ -1,10 +1,13 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MerchandiseTest {
 
     @Test
     public void initializeMerchandiseWithPrice() {
         Merchandise merchandise = new Merchandise(100);
         int price = merchandise.getPrice();
+        assertEquals(100, price);
     }
 }
