09ebfcc5f7084f8aaf9fb19cbead11a4d012df07
Matt Chowning <mchowning@gmail.com>
2016-11-30 20:40:58 -0500

    GREEN | return pencil promotion if there was a previous price

--- src/main/java/Merchandise.java
+++ src/main/java/Merchandise.java
@@ -1,9 +1,13 @@
 public class Merchandise {
 
+    private static final Double UNINITIALIZED_PRICE = -1.0;
+
+    private double previousPrice;
     private double price;
 
     public Merchandise(double price) {
         this.price = price;
+        this.previousPrice = UNINITIALIZED_PRICE;
     }
 
     public double getPrice() {
@@ -11,10 +15,11 @@ public class Merchandise {
     }
 
     public boolean isRedPencilPromotion() {
-        return false;
+        return previousPrice != UNINITIALIZED_PRICE;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setPrice(double newPrice) {
+        previousPrice = price;
+        price = newPrice;
     }
 }
