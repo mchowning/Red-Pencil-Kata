0460ea4442541e20bef4bf47626bc4357e1968a8
Matt Chowning <mchowning@gmail.com>
2016-11-30 23:40:30 -0500

    REFACTOR | extract method for handling percent price reduced

--- src/main/java/Merchandise.java
+++ src/main/java/Merchandise.java
@@ -22,15 +22,19 @@ public class Merchandise {
         if (previousPrice == UNINITIALIZED_PRICE) {
             return false;
         } else {
-            BigDecimal bigPreviousPrice = BigDecimal.valueOf(previousPrice);
-            BigDecimal bigPrice = BigDecimal.valueOf(price);
-            BigDecimal percentOfPrevious = bigPrice.divide(bigPreviousPrice,
-                                                           2,
-                                                           BigDecimal.ROUND_HALF_UP);
-            double percentReduced = BigDecimal.ONE.subtract(percentOfPrevious).doubleValue();
+            double percentReduced = getPercentPriceReduced();
             return percentReduced >= PENCIL_PROMOTION_MIN_REDUCTION_PERCENT &&
                     percentReduced <= PENCIL_PROMOTION_MAX_REDUCTION_PERCENT;
         }
+    }
+
+    private double getPercentPriceReduced() {
+        BigDecimal bigPreviousPrice = BigDecimal.valueOf(previousPrice);
+        BigDecimal bigPrice = BigDecimal.valueOf(price);
+        BigDecimal percentOfPrevious = bigPrice.divide(bigPreviousPrice,
+                                                       2,
+                                                       BigDecimal.ROUND_HALF_UP);
+        return BigDecimal.ONE.subtract(percentOfPrevious).doubleValue();
     }
 
     public void setPrice(double newPrice) {
