a3103684cb359c8939ad513029b84a77f08bdce4
Matt Chowning <mchowning@gmail.com>
2016-12-01 08:57:27 -0500

    REFACTOR | remove previousPrice field

--- src/main/java/Merchandise.java
+++ src/main/java/Merchandise.java
@@ -12,11 +12,10 @@ public class Merchandise {
     private static final int DAYS_FOR_STABLE_PRICE = 30;
     private static final int MAX_PENCIL_PROMO_DURATION_DAYS = 30;
 
-    @Nullable private Price previousPrice;
     private Price price;
 
     public Merchandise(double price) {
-        this.price = new Price(price);
+        setPrice(price);
     }
 
     public double getPrice() {
@@ -24,46 +23,59 @@ public class Merchandise {
     }
 
     public boolean isRedPencilPromo() {
-        if (isPreviousPriceStable()) {
-            double percentReduced = getPercentPriceReduced();
-            boolean isRedPencilReductionAmount = percentReduced >= PENCIL_PROMO_MIN_REDUCTION_PERCENT
-                                                   && percentReduced <= PENCIL_PROMO_MAX_REDUCTION_PERCENT;
-            boolean isUnexpired = price.time.plusDays(MAX_PENCIL_PROMO_DURATION_DAYS).isAfterNow()
-                                    || price.time.plusDays(MAX_PENCIL_PROMO_DURATION_DAYS).isEqualNow();
-            return isRedPencilReductionAmount && isUnexpired;
+        if (price.redPencilPromoExpiration != null) {
+            boolean isUnexpired = price.redPencilPromoExpiration.isAfterNow()
+                    || price.redPencilPromoExpiration.isEqualNow();
+            return isUnexpired;
         }
         return false;
     }
 
-    private double getPercentPriceReduced() {
-        BigDecimal bigPreviousPrice = BigDecimal.valueOf(previousPrice.amount);
-        BigDecimal bigPrice = BigDecimal.valueOf(price.amount);
-        BigDecimal percentOfPrevious = bigPrice.divide(bigPreviousPrice,
-                                                       2,
-                                                       BigDecimal.ROUND_HALF_UP);
-        return BigDecimal.ONE.subtract(percentOfPrevious).doubleValue();
-    }
-
-    private boolean isPreviousPriceStable() {
-        return previousPrice != null &&
-                !previousPrice.time
-                              .plusDays(DAYS_FOR_STABLE_PRICE)
-                              .isAfter(price.time.toInstant());
-    }
-
     public void setPrice(double newPrice) {
-        previousPrice = price;
-        price = new Price(newPrice);
+        DateTime now = DateTime.now();
+        DateTime redPencilPromoExpiration = getRedPencilPromoExpiration(newPrice, now);
+        price = new Price(newPrice, now, redPencilPromoExpiration);
+    }
+
+    @Nullable
+    private DateTime getRedPencilPromoExpiration(double newPrice, DateTime now) {
+        DateTime redPencilPromoExpiration = null;
+        if (price != null && isCurrentPriceStableAsOf(price.time, now) && isRedPencilPromoPriceChange(price.amount, newPrice)) {
+            redPencilPromoExpiration = now.plusDays(MAX_PENCIL_PROMO_DURATION_DAYS);
+        }
+        return redPencilPromoExpiration;
+    }
+
+    private boolean isCurrentPriceStableAsOf(DateTime currentPriceStartTime, DateTime time) {
+        return !currentPriceStartTime.plusDays(DAYS_FOR_STABLE_PRICE)
+                                     .isAfter(time);
+    }
+
+    private boolean isRedPencilPromoPriceChange(double oldPrice, double newPriceAmount) {
+        double percentReduced = getPercentPriceReduced(oldPrice, newPriceAmount);
+        return percentReduced >= PENCIL_PROMO_MIN_REDUCTION_PERCENT
+                && percentReduced <= PENCIL_PROMO_MAX_REDUCTION_PERCENT;
+    }
+
+    private double getPercentPriceReduced(double originalPriceAmount, double newPriceAmount) {
+        BigDecimal bigCurrentPrice = BigDecimal.valueOf(originalPriceAmount);
+        BigDecimal bigNewPrice = BigDecimal.valueOf(newPriceAmount);
+        BigDecimal percentOfCurrent = bigNewPrice.divide(bigCurrentPrice,
+                                                          2,
+                                                          BigDecimal.ROUND_HALF_UP);
+        return BigDecimal.ONE.subtract(percentOfCurrent).doubleValue();
     }
 
     private static class Price {
 
         private final double amount;
         @NonNull private final DateTime time;
+        @Nullable private final DateTime redPencilPromoExpiration;
 
-        private Price(double amount) {
+        private Price(double amount, @NonNull DateTime priceDate, @Nullable DateTime redPencilPromoExpiration) {
             this.amount = amount;
-            this.time = DateTime.now();
+            this.time = priceDate;
+            this.redPencilPromoExpiration = redPencilPromoExpiration;
         }
     }
 }
