ad34f0a89c25324a7975e3afd68c0514c45d60e2
Matt Chowning <mchowning@gmail.com>
2016-12-03 16:40:47 -0500

    REFACTOR | extract common test setup

--- src/test/java/MerchandiseTest.java
+++ src/test/java/MerchandiseTest.java
@@ -14,10 +14,12 @@ public class MerchandiseTest {
     private static final double DOUBLE_TEST_DELTA = 0.001;
 
     private Merchandise subject;
+    private RedPencilPromoChecker promoChecker;
 
     @Before
     public void setUp() {
-        subject = new Merchandise(INITIAL_PRICE, new RedPencilPromoChecker());
+        promoChecker = mock(RedPencilPromoChecker.class);
+        subject = new Merchandise(INITIAL_PRICE, promoChecker);
     }
 
     @Test
@@ -27,24 +29,18 @@ public class MerchandiseTest {
 
     @Test
     public void notifiesPromoCheckerOfPriceChanges() {
-        RedPencilPromoChecker promoChecker = mock(RedPencilPromoChecker.class);
-        subject = new Merchandise(INITIAL_PRICE, promoChecker);
         verify(promoChecker).notifyOfPriceUpdate(INITIAL_PRICE);
     }
 
     @Test
     public void returnsIsPromoIfPromoCheckerReturnsTrue() {
-        RedPencilPromoChecker promoChecker = mock(RedPencilPromoChecker.class);
         when(promoChecker.isPromoActive()).thenReturn(true);
-        subject = new Merchandise(INITIAL_PRICE, promoChecker);
         assertTrue(subject.isRedPencilPromo());
     }
 
     @Test
     public void returnsIsNotPromoIfPromoCheckerReturnsFalse() {
-        RedPencilPromoChecker promoChecker = mock(RedPencilPromoChecker.class);
         when(promoChecker.isPromoActive()).thenReturn(false);
-        subject = new Merchandise(INITIAL_PRICE, promoChecker);
         assertFalse(subject.isRedPencilPromo());
     }
 }
