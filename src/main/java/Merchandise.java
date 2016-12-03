ac69f0565ad11913229595b687974a6cdf5a8142
Matt Chowning <mchowning@gmail.com>
2016-12-03 16:37:15 -0500

    REFACTOR | remove no-longer-needed/duplicated code

--- src/main/java/Merchandise.java
+++ src/main/java/Merchandise.java
@@ -1,19 +1,9 @@
-import org.joda.time.DateTime;
-
-import java.math.BigDecimal;
-
 import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
 
 public class Merchandise {
 
-    private static final Double PENCIL_PROMO_MIN_REDUCTION_PERCENT = 0.05;
-    private static final Double PENCIL_PROMO_MAX_REDUCTION_PERCENT = 0.30;
-    private static final int DAYS_FOR_STABLE_PRICE = 30;
-    private static final int MAX_PENCIL_PROMO_DURATION_DAYS = 30;
-
     private final RedPencilPromoChecker promoChecker;
-    private Price price;
+    private double price;
 
     public Merchandise(double price, @NonNull RedPencilPromoChecker promoChecker) {
         this.promoChecker = promoChecker;
@@ -21,87 +11,15 @@ public class Merchandise {
     }
 
     public double getPrice() {
-        return price.amount;
+        return price;
     }
 
     public boolean isRedPencilPromo() {
         return promoChecker.isPromoActive();
     }
 
-    public void setPrice(double newPrice) {
-        promoChecker.notifyOfPriceUpdate(newPrice);
-        DateTime now = DateTime.now();
-        RedPencilPromo redPencilPromo = getRedPencilPromo(newPrice, now);
-        price = new Price(newPrice, now, redPencilPromo);
-    }
-
-    @Nullable
-    private RedPencilPromo getRedPencilPromo(double newPrice, DateTime now) {
-        if (shouldContinuePromo(newPrice)) {
-            return price.redPencilPromo;
-        } else if (shouldStartNewPromo(newPrice)) {
-            DateTime expiration = now.plusDays(MAX_PENCIL_PROMO_DURATION_DAYS)
-                                     .plusMillis(1);
-            return new RedPencilPromo(price.amount, expiration);
-        }
-        return null;
-    }
-
-    private boolean shouldContinuePromo(double newPrice) {
-        return price != null &&
-                price.redPencilPromo != null &&
-                newPrice <= price.amount &&
-                isRedPencilPromo() &&
-                isRedPencilPromoPriceChange(price.redPencilPromo.prePromoPrice, newPrice);
-    }
-
-    private boolean shouldStartNewPromo(double newPrice) {
-        return price != null &&
-                isCurrentPriceStable() &&
-                isRedPencilPromoPriceChange(price.amount, newPrice);
-    }
-
-    private boolean isCurrentPriceStable() {
-        DateTime timeForStability = price.time.plusDays(DAYS_FOR_STABLE_PRICE);
-        return timeForStability.isBeforeNow() || timeForStability.isEqualNow();
-    }
-
-    private boolean isRedPencilPromoPriceChange(double oldPrice, double newPriceAmount) {
-        double percentReduced = getPercentPriceReduced(oldPrice, newPriceAmount);
-        return percentReduced >= PENCIL_PROMO_MIN_REDUCTION_PERCENT
-                && percentReduced <= PENCIL_PROMO_MAX_REDUCTION_PERCENT;
-    }
-
-    private double getPercentPriceReduced(double originalPriceAmount, double newPriceAmount) {
-        BigDecimal bigCurrentPrice = BigDecimal.valueOf(originalPriceAmount);
-        BigDecimal bigNewPrice = BigDecimal.valueOf(newPriceAmount);
-        BigDecimal percentOfCurrent = bigNewPrice.divide(bigCurrentPrice,
-                                                         2,
-                                                         BigDecimal.ROUND_HALF_UP);
-        return BigDecimal.ONE.subtract(percentOfCurrent).doubleValue();
-    }
-
-    private static class Price {
-
-        private final double amount;
-        @NonNull private final DateTime time;
-        @Nullable private final RedPencilPromo redPencilPromo;
-
-        private Price(double amount, @NonNull DateTime priceDate, @Nullable RedPencilPromo redPencilPromo) {
-            this.amount = amount;
-            this.time = priceDate;
-            this.redPencilPromo = redPencilPromo;
-        }
-    }
-
-    private static class RedPencilPromo {
-
-        private final double prePromoPrice;
-        @NonNull private final DateTime expiration;
-
-        private RedPencilPromo(double prePromoPrice, @NonNull DateTime expiration) {
-            this.prePromoPrice = prePromoPrice;
-            this.expiration = expiration;
-        }
+    public void setPrice(double price) {
+        this.price = price;
+        promoChecker.notifyOfPriceUpdate(price);
     }
 }
